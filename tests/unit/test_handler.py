d3909e9ec7d3847efadc89b2d5696ae4c87f310a
studio3104 <studio3104.com@gmail.com>
2020-03-19 11:59:45 +0900

    Change the key name to refer requests paths

--- tests/unit/test_handler.py
+++ tests/unit/test_handler.py
@@ -8,7 +8,7 @@ from app import app
 
 class TestDispatchRequestGeneral:
     def test_404(self, apigw_event) -> None:
-        apigw_event['requestContext']['path'] = '/NOT_IMPLEMENTED_PATH'
+        apigw_event['requestContext']['resourcePath'] = '/NOT_IMPLEMENTED_PATH'
         response = app.dispatch_request(apigw_event, {})
         assert response['statusCode'] == HTTPStatus.NOT_FOUND
 
@@ -16,7 +16,7 @@ class TestDispatchRequestGeneral:
         'POST', 'OPTIONS', 'CONNECT', 'TRACE',
     ))
     def test_405(self, apigw_event, not_allowed_method: str) -> None:
-        apigw_event['requestContext']['path'] = '/user'
+        apigw_event['requestContext']['resourcePath'] = '/user'
         apigw_event['httpMethod'] = not_allowed_method
         response = app.dispatch_request(apigw_event, {})
         assert response['statusCode'] == HTTPStatus.METHOD_NOT_ALLOWED
@@ -25,7 +25,7 @@ class TestDispatchRequestGeneral:
 class TestDispatchRequestGet:
     @pytest.fixture
     def event(self, apigw_event, fx_dummy_user):  # TODO: Specify type
-        apigw_event['requestContext']['path'] = '/user/{user_id}'
+        apigw_event['requestContext']['resourcePath'] = '/user/{user_id}'
         apigw_event['httpMethod'] = 'GET'
         apigw_event['pathParameters']['user_id'] = fx_dummy_user['user_id']
         return apigw_event
@@ -45,7 +45,7 @@ class TestDispatchRequestGet:
 class TestDispatchRequestPut:
     @pytest.fixture
     def event(self, apigw_event):  # TODO: Specify type
-        apigw_event['requestContext']['path'] = '/user'
+        apigw_event['requestContext']['resourcePath'] = '/user'
         apigw_event['httpMethod'] = 'PUT'
         apigw_event['body'] = json.dumps({'name': 'fatsushi'})
         return apigw_event
@@ -60,7 +60,7 @@ class TestDispatchRequestPut:
 class TestDispatchRequestDelete:
     @pytest.fixture
     def event(self, apigw_event, fx_dummy_user):  # TODO: Specify type
-        apigw_event['requestContext']['path'] = '/user/{user_id}'
+        apigw_event['requestContext']['resourcePath'] = '/user/{user_id}'
         apigw_event['httpMethod'] = 'DELETE'
         apigw_event['pathParameters']['user_id'] = fx_dummy_user['user_id']
         return apigw_event
@@ -78,7 +78,7 @@ class TestDispatchRequestDelete:
 class TestDispatchRequestPatch:
     @pytest.fixture
     def event(self, apigw_event, fx_dummy_user):  # TODO: Specify type
-        apigw_event['requestContext']['path'] = '/user/{user_id}'
+        apigw_event['requestContext']['resourcePath'] = '/user/{user_id}'
         apigw_event['httpMethod'] = 'PATCH'
         apigw_event['pathParameters']['user_id'] = fx_dummy_user['user_id']
         return apigw_event
